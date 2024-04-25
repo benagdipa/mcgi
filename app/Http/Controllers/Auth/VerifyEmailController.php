04b34e81a4045d2e71decca4fc172a50b7fd2ba1
SanjayPCSoft <113160895+SanjayPCSoft@users.noreply.github.com>
2024-04-25 11:24:29 +0545

    Redirect verified guest user to home

--- app/Http/Controllers/Auth/VerifyEmailController.php
+++ app/Http/Controllers/Auth/VerifyEmailController.php
@@ -16,13 +16,18 @@ class VerifyEmailController extends Controller
     public function __invoke(EmailVerificationRequest $request): RedirectResponse
     {
         if ($request->user()->hasVerifiedEmail()) {
-            return redirect()->intended(RouteServiceProvider::HOME.'?verified=1');
+            return redirect()->intended(RouteServiceProvider::HOME . '?verified=1');
         }
 
         if ($request->user()->markEmailAsVerified()) {
             event(new Verified($request->user()));
         }
 
-        return redirect()->intended(RouteServiceProvider::HOME.'?verified=1');
+        $roles = $request->user()->roles->pluck('name')->toArray();
+
+        if (in_array('guest', $roles)) {
+            return redirect()->intended('/');
+        }
+        return redirect()->intended(RouteServiceProvider::HOME . '?verified=1');
     }
 }
