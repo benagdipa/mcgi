6b55072f357c6ad678439fc6b055bf883c4bc23a
SanjayPCSoft <sanjay.karki@pcsoftnepal.com>
2024-04-25 13:37:09 +0545

    adding loading icon before loaded the pages

--- resources/js/Layouts/GuestLayout.jsx
+++ resources/js/Layouts/GuestLayout.jsx
@@ -1,14 +1,28 @@
 import Footer from '@/Components/Footer';
 import Header from '@/Components/Header';
-import React from 'react';
+import React,{useEffect,useState} from 'react';
 export default function Guest({ user, children }) {
+    const [isLoading, setIsLoading] = useState(true);
+
+    useEffect(() => {
+        const timer = setTimeout(() => {
+            setIsLoading(false);
+        }, 2000);
+        return () => clearTimeout(timer);
+    }, []);
     return (
         <React.Fragment>
+               {isLoading ? (
+             <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', minHeight: '100vh' }}>
+                <img src='../images/loading-effect.gif' alt='Loading........' className = 'spinner text-center' />
+            </div>
+            ) :( <>
             <Header user={user} />
             <div className="page-content">
                 {children}
             </div>
             <Footer />
+            </>)}
         </React.Fragment>
     );
 }
