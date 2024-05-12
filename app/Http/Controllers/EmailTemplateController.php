c1be9ffef519343cd12e49bbf6591f1d3491b33a
SanjayPCSoft <113160895+SanjayPCSoft@users.noreply.github.com>
2024-05-12 11:46:34 +0545

    Added Email template edit and delete

--- app/Http/Controllers/EmailTemplateController.php
+++ app/Http/Controllers/EmailTemplateController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\EmailTemplate;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Redirect;
 use Inertia\Inertia;
 use Illuminate\Support\Facades\Mail;
 
@@ -33,5 +34,39 @@ class EmailTemplateController extends Controller
             return to_route('admin.email.index');
         }
 
+    }
+    public function admin_update(Request $request, $id)
+    {
+        $request->validate([
+            'name' => 'required|string|max:255',
+            'subject' => 'required|string|max:255',
+            'content' => 'required|string',
+        ]);
+        $template = EmailTemplate::findOrFail($id);
+        if ($template) {
+            $template->update([
+                'name' => $request->input('name'),
+                'subject' => $request->input('subject'),
+                'content' => $request->input('content'),
+            ]);
+            if ($template) {
+                return to_route('admin.email.index');
+            }
+        }
+    }
+
+    public function admin_delete($id)
+    {
+        if ($id !== '1') {
+            $template = EmailTemplate::findOrFail($id);
+            if ($template) {
+                $template->delete();
+                return to_route('admin.email.index');
+            } else {
+                return to_route('admin.email.index')->with('message', 'Email template not found.');
+            }
+        } else {
+            return to_route('admin.email.index')->with('message', 'This Email template cannot be deleted. It is used in Attendance email.');
+        }
     }
 }
